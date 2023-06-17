431400bc303fc7fdcbc408216bbcd49bebc5feb1
Gaurav Sablok <sablokg@gmail.com>
2023-06-17 13:45:11 +0530

    Update bash_reference_manual_for_quick_data_analysis.sh

--- bash_reference_manual_for_quick_data_analysis.sh
+++ bash_reference_manual_for_quick_data_analysis.sh
@@ -4,8 +4,12 @@ echo $(pwd)
 # using the cut on the comma delimiter files the sample data can be found in the main directory named iris
 cat iris.csv| cut -f 1 -d "," # you can use any delimited here
 # if you want to select from the first column and then select the numbers starting with the specific number or the alphanumeral
-cat iris.csv| cut -f 1 -d "," | grep "^4" # here you can also use the string as 
+cat iris.csv| cut -f 1 -d "," | grep "^4"
+# string sort based on the start of the string
 cat iris.csv| cut -f 1 -d "," | grep "^Pretoria" 
+# alphanumeral sort based on the end of the string
+cat iris.csv | cut -f 1 -d "," | grep "7$"
+
 # PBS alloc file 
 
 
